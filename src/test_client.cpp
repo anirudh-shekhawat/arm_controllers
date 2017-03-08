9fe09a09207fa432a2e5cac1a825bc7eb3656b3b
root <root@ggn000267.greyorange.com>
2017-03-08 17:22:37 +0530

    get states added

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -1,5 +1,7 @@
 #include <ros/ros.h>
 #include "gazebo_msgs/SetModelState.h"
+#include "gazebo_msgs/GetModelState.h"
+
 
 using namespace std;
 
@@ -11,13 +13,13 @@ ros::NodeHandle n;
 
 
 geometry_msgs::Pose start_pose;
-start_pose.position.x = 0.0;
+start_pose.position.x = 1.0;
 start_pose.position.y = 0.0;
-start_pose.position.z = 0.1;
+start_pose.position.z = 0.0;
 start_pose.orientation.x = 0.0;
 start_pose.orientation.y = 0.0;
 start_pose.orientation.z = 0.0;
-start_pose.orientation.w = 0.0;
+start_pose.orientation.w = 1.0;
 
 geometry_msgs::Twist start_twist;
 start_twist.linear.x = 0.0;
@@ -46,6 +48,23 @@ setmodelstate.request.model_state = modelstate;
 
        else{
               ROS_ERROR("Failed to magic move PR2! Error msg:%s",setmodelstate.response.status_message.c_str());
+ 
+         }
+
+geometry_msgs::Pose final_pose;
+geometry_msgs::Twist final_twist;
+gazebo_msgs::GetModelState getmodelstate;
+ros::ServiceClient gmscl=n.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+getmodelstate.request.model_name ="rrbot";
+        if(gmscl.call(getmodelstate)){
+
+              ROS_INFO("GET STATE success!!");
+              final_pose=getmodelstate.response.pose;
+              final_twist=getmodelstate.response.twist; 
+              ROS_INFO("position-x-%f",(final_pose.position.x+0.0));
+         }
+       else{
+              ROS_ERROR("GET STATE FAILED:%s",getmodelstate.response.status_message.c_str());
  
          }
 
