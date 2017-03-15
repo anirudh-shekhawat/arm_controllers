0eaa194936288a3c6cbf264c437757b32a23b63e
root <root@ggn000267.greyorange.com>
2017-03-15 19:27:39 +0530

    added moveit execution related files

--- src/arm_move.cpp
+++ src/arm_move.cpp
@@ -20,16 +20,29 @@ int main(int argc, char **argv)
   spinner.start();
 
   static const std::string PLANNING_GROUP = "manipulator"; //Changed from right_arm
-
+ROS_INFO_NAMED("arm_move","STARTING MOVEMENT SCRIPT");
   // The :move_group_interface:`MoveGroup` class can be easily
   // setup using just the name of the planning group you would like to control and plan for.		         
   moveit::planning_interface::MoveGroup move_group(PLANNING_GROUP);
+
+  const robot_state::JointModelGroup *joint_model_group =
+  move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  ROS_INFO_NAMED("arm_move", "Reference frame: %s", move_group.getPlanningFrame().c_str());
+  ROS_INFO_NAMED("arm_move", "End effector link: %s", move_group.getEndEffectorLink().c_str());
+
     geometry_msgs::Pose target_pose1;
   target_pose1.orientation.w = 1.0;
   target_pose1.position.x = 0.28;
   target_pose1.position.y = -0.7;
   target_pose1.position.z = 1.0;
   move_group.setPoseTarget(target_pose1);
+
+moveit::planning_interface::MoveGroup::Plan my_plan;
+
+bool success = move_group.plan(my_plan);
+
+ROS_INFO_NAMED("arm_move", " plan 1 (pose goal) %s", success ? "" : "FAILED");
 
   move_group.move();
 
